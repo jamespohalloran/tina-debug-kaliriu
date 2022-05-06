87cdcab81aa5a8b0533bc148f1ebb35db0a8999c
palomachiara <60576212+palomachiara@users.noreply.github.com>
2022-05-06 19:37:27 +0200

    cursor try 2

--- themes/kaliriu-theme/static/js/scripts.js
+++ themes/kaliriu-theme/static/js/scripts.js
@@ -63,3 +63,25 @@ cookieButton.addEventListener('click', function(e){
     anchors[i].setAttribute('rel', 'noreferrer nofollow');
   }
 }
+
+/* CURSOR */
+const cursor = document.querySelector('.cursor');
+let mouseX = 0;
+let mouseY = 0;
+let cursorX = 0;
+let cursorY = 0;
+let speed = 0.5; // change to increase the ease
+function animate() {
+    let distX = mouseX - cursorX;
+    let distY = mouseY - cursorY;
+    cursorX = cursorX + (distX * speed);
+    cursorY = cursorY + (distY * speed);
+    cursor.style.left = cursorX + 'px';
+    cursor.style.top = cursorY + 'px';
+    requestAnimationFrame(animate);
+}
+animate();
+document.addEventListener('mousemove', (event) => {
+    mouseX = event.pageX;
+    mouseY = event.pageY;
+})
